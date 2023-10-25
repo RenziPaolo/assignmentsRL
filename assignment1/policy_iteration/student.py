ff9ecc37d9bef442e4f8f8492dabd286622a3577
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2023-10-25 18:31:18 +0200

    fix on student

--- assignment1/policy_iteration/student.py
+++ assignment1/policy_iteration/student.py
@@ -1,21 +1,25 @@
 import numpy as np
-
+import gymnasium as gym
+from math import sqrt
 
 # TODO: Implement the reward function as described in Gymnasium documentation.
 # The end-state is always at [env_size-1,env_size-1].
 def reward_function(s, env_size):
-    r = ... # ?
+    if s[0] == env_size-1 or s[1] == env_size-1:
+        r = 1 
+    else:
+        r = 0
 
     return r
 
 # do not modify this function
-def reward_probabilities(env):
-    rewards = np.zeros((env.observation_space.n))
+def reward_probabilities(env_size):
+    rewards = np.zeros(env_size*env_size)
     i = 0
-    for r in range(env.observation_space.n):
-        for c in range(env.observation_space.n):
+    for r in range(env_size):
+        for c in range(env_size):
             state = np.array([r,c], dtype=np.uint8)
-            rewards[i] = reward_function(state, env.observation_space.n)
+            rewards[i] = reward_function(state, env_size*env_size)           
             i+=1
 
     return rewards
@@ -23,9 +27,11 @@ def reward_probabilities(env):
 # Check feasibility of the new state.
 # If it is a possible state return s_prime, otherwise return s
 def check_feasibility(s_prime, s, env_size, obstacles):
-  # TODO
-
-  return s
+    if s_prime[0]<0 or s_prime[1]<0 or s_prime[0]>=env_size or s_prime[1]>=env_size:
+        return s
+    if obstacles[s_prime[0],s_prime[1]] == 0:
+        return s
+    return s_prime
 
 def transition_probabilities(env, s, a, env_size, directions, obstacles):
     cells = []
@@ -35,13 +41,15 @@ def transition_probabilities(env, s, a, env_size, directions, obstacles):
     # TODO
     # Fill in the cells corresponding to the next possible states with the probability of visiting each of them
     # Remember to check the feasibility of each new state!
-    s_prime = ... # ??
-    prob_next_state[s_prime[0], s_prime[1]] = ... # ???
 
-    s_prime = ... # ??
-    prob_next_state[s_prime[0], s_prime[1]] = ... # ???
+    s_prime = check_feasibility(s + directions[a], s, env_size, obstacles)
+    prob_next_state[s_prime[0], s_prime[1]] = 1/3 
 
-    s_prime = ... # ??
-    prob_next_state[s_prime[0], s_prime[1]] = ... # ???
+    s_prime = check_feasibility(s + [directions[a][0],-directions[a][0]], s, env_size, obstacles)
+    prob_next_state[s_prime[0], s_prime[1]] = 1/3 
+
+    s_prime = check_feasibility(s + [-directions[a][0],directions[a][0]], s, env_size, obstacles)
+    print(s,s_prime)
+    prob_next_state[s_prime[0], s_prime[1]] = 1/3 
 
     return prob_next_state
