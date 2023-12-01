03b03cec19899e66c3f160a9a1e6bf0445fee6c2
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2023-12-01 16:07:40 +0100

    Cspell removal + start of development on world model

--- assignment3/car_racing/student.py
+++ assignment3/car_racing/student.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch import Tensor, List
 
 class Policy(nn.Module):
     continuous = False # you can change this
@@ -11,32 +12,46 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         self.device = device
         #TODO 
-        #self.VAE
-        #self.MDN-RNN
-        #self.C
+        self.VAE = VAE(in_channels = 96*96*3, latent_dim = 100, hidden_dims = None)
+        self.MDN_RNN = MDN_RNN(input_size = 100+3+10, hidden_size = 5)
+        self.C = nn.linear()
 
     def forward(self, x):
         # TODO
-        #z, _ = V(x)
-        #h = MDN-RNN(z, a, h)
-        #a = C(z,h)
+        z = self.VAE.encode(x)      
+        h = self.MDN_RNN(z, a, h)
+        a = self.C(z, h)
 
-        return x
+        return a
     
     def act(self, state):
         # TODO
         #z,_ = V(state)
-        #h = MDN-RNN(z, a, h)
+        #h = MDN_RNN(z, a, h)
         #a = C(z,h)
 
         return 
 
     def train(self):
         # TODO
+        #first initialization / skip if load from file
+        #rollout = []
+        #for _ in range(10000):
+        #   a = env.ActionSpace.sample()
+        #   observation, reward, terminated, truncated, info = env.step(a)
+        #   rollout.append(observation)
         #VAE.train()
-        #MDN-RNN.train()
+        #MDN_RNN.train()
         #CMA-ES(C).train
-
+        # for _ in range(10):
+            # rollout = []
+            # for _ in range(10000):
+            # a = C.action()
+            # observation, reward, terminated, truncated, info = env.step(a)
+            # rollout.appned(observation)
+            # VAE.train()
+            # MDN_RNN.train()
+            # CMA-ES(C).train
 
         return
 
@@ -44,9 +59,195 @@ class Policy(nn.Module):
         torch.save(self.state_dict(), 'model.pt')
 
     def load(self):
-        self.load_state_dict(torch.load('model.pt'), map_location=self.device)
+        self.load_state_dict(torch.load('model.pt', map_location=self.device))
 
     def to(self, device):
         ret = super().to(device)
         ret.device = device
         return ret
+
+class VAE(nn.Module):
+    #Variational Auto Encoder Class
+    
+
+    def __init__(self,
+                 in_channels: int,
+                 latent_dim: int,
+                 hidden_dims: List = None,
+                 **kwargs) -> None:
+        super(self).__init__()
+
+        self.latent_dim = latent_dim
+
+        modules = []
+        if hidden_dims is None:
+            hidden_dims = [32, 64, 128, 256, 512]
+
+        # Build Encoder
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim,
+                              kernel_size= 3, stride= 2, padding  = 1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
+
+        self.encoder = nn.Sequential(*modules)
+        self.fc_mu = nn.Linear(hidden_dims[-1]*4, latent_dim)
+        self.fc_var = nn.Linear(hidden_dims[-1]*4, latent_dim)
+
+
+        # Build Decoder
+        modules = []
+
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 4)
+
+        hidden_dims.reverse()
+
+        for i in range(len(hidden_dims) - 1):
+            modules.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(hidden_dims[i],
+                                       hidden_dims[i + 1],
+                                       kernel_size=3,
+                                       stride = 2,
+                                       padding=1,
+                                       output_padding=1),
+                    nn.BatchNorm2d(hidden_dims[i + 1]),
+                    nn.LeakyReLU())
+            )
+
+
+
+        self.decoder = nn.Sequential(*modules)
+
+        self.final_layer = nn.Sequential(
+                            nn.ConvTranspose2d(hidden_dims[-1],
+                                               hidden_dims[-1],
+                                               kernel_size=3,
+                                               stride=2,
+                                               padding=1,
+                                               output_padding=1),
+                            nn.BatchNorm2d(hidden_dims[-1]),
+                            nn.LeakyReLU(),
+                            nn.Conv2d(hidden_dims[-1], out_channels= 3,
+                                      kernel_size= 3, padding= 1),
+                            nn.Tanh())
+
+    def encode(self, input: Tensor) -> List[Tensor]:
+        """
+        Encodes the input by passing through the encoder network
+        and returns the latent codes.
+        :param input: (Tensor) Input tensor to encoder [N x C x H x W]
+        :return: (Tensor) List of latent codes
+        """
+        result = self.encoder(input)
+        result = torch.flatten(result, start_dim=1)
+
+        # Split the result into mu and var components
+        # of the latent Gaussian distribution
+        mu = self.fc_mu(result)
+        log_var = self.fc_var(result)
+
+        return [mu, log_var]
+
+    def decode(self, z: Tensor) -> Tensor:
+        """
+        Maps the given latent codes
+        onto the image space.
+        :param z: (Tensor) [B x D]
+        :return: (Tensor) [B x C x H x W]
+        """
+        result = self.decoder_input(z)
+        result = result.view(-1, 512, 2, 2)
+        result = self.decoder(result)
+        result = self.final_layer(result)
+        return result
+
+    def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
+        """
+        Reparameterization trick to sample from N(mu, var) from
+        N(0,1).
+        :param mu: (Tensor) Mean of the latent Gaussian [B x D]
+        :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
+        :return: (Tensor) [B x D]
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
+    def forward(self, input: Tensor, **kwargs) -> List[Tensor]:
+        mu, log_var = self.encode(input)
+        z = self.reparameterize(mu, log_var)
+        return  [self.decode(z), input, mu, log_var]
+
+    def loss_function(self,
+                      *args,
+                      **kwargs) -> dict:
+        """
+        Computes the VAE loss function.
+        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        recons = args[0]
+        input = args[1]
+        mu = args[2]
+        log_var = args[3]
+
+        kld_weight = kwargs['M_N'] # Account for the minibatch samples from the dataset
+        recons_loss =F.mse_loss(recons, input)
+
+
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+
+        loss = recons_loss + kld_weight * kld_loss
+        return {'loss': loss, 'Reconstruction_Loss':recons_loss.detach(), 'KLD':-kld_loss.detach()}
+
+    def sample(self,
+               num_samples:int,
+               current_device: int, **kwargs) -> Tensor:
+        """
+        Samples from the latent space and return the corresponding
+        image space map.
+        :param num_samples: (Int) Number of samples
+        :param current_device: (Int) Device to run the model
+        :return: (Tensor)
+        """
+        z = torch.randn(num_samples,
+                        self.latent_dim)
+
+        z = z.to(current_device)
+
+        samples = self.decode(z)
+        return samples
+
+    def generate(self, x: Tensor, **kwargs) -> Tensor:
+        """
+        Given an input image x, returns the reconstructed image
+        :param x: (Tensor) [B x C x H x W]
+        :return: (Tensor) [B x C x H x W]
+        """
+
+        return self.forward(x)[0]
+
+
+class MDN_RNN(nn.Module):
+    #Mixture Density Network Recurrent neural network
+    def __init__(self, input_size, hidden_size, num_layers, dropout):
+        super(self).__init__()
+        self.RNN = nn.RNN(input_size, hidden_size, num_layers, dropout)
+        mix = torch.D.Categorical(torch.ones(5,))
+        comp = torch.D.Normal(torch.randn(5,), torch.rand(5,))
+        self.gmm = torch.MixtureSameFamily(mix, comp)
+        self.RNN_MDN = nn.Sequential(self.RNN, self.gmm) 
+
+    def forward(self, h, a, z):
+        
+        return self.RNN_MDN(np.array([h, a, z]))
+    
+    def hiddenState(self, h, a, z):
+        return 
